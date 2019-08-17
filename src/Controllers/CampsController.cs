2c7019da40c6d50633e84919e6a63ad7666a9fad
Raul Morales <irmoralesb@hotmail.com>
2019-08-17 12:55:30 -0500

    Module 1 WIP. Error thrown when includeTalks flag is true.

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -1,4 +1,9 @@
 using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreCodeCamp.Controllers{
@@ -6,12 +11,47 @@ namespace CoreCodeCamp.Controllers{
     [Route("api/[controller]")]
     public class CampsController : ControllerBase
     {
-        
-        
-        public  object Get()
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CampsController(ICampRepository repository, IMapper mapper)
         {
-            return new { Moniker= "ATL2018", Name = "Atlanta Code Camp"};
+            _repository = repository;
+            _mapper = mapper;
         }
+        
+        [HttpGet]
+        public async Task<ActionResult<CampModel[]>> Get(bool includeTalks = false)
+        {
+            try
+            {
+                var results = await _repository.GetAllCampsAsync(includeTalks);
+                CampModel[] models = _mapper.Map<CampModel[]>(results);
+
+                return Ok(models);
+            }catch(Exception)
+            {
+                return this.StatusCode( StatusCodes.Status500InternalServerError, "Database failure");
+            }
+        }
+
+        [HttpGet("{moniker}")]
+        public async Task<ActionResult<CampModel>> Get(string moniker)
+        {
+            try
+            {
+                var result = await _repository.GetCampAsync(moniker);
+                if(result == null)
+                { return NotFound(); }
+
+                CampModel model = _mapper.Map<CampModel>(result);
+                return Ok(model);
+            }catch(Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Error");
+            }
+        }
+        
 
     }
 }
---
+++ src/Data/CampProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using CoreCodeCamp.Models;
+
+namespace CoreCodeCamp.Data
+{
+    public class CampProfile : Profile
+    {
+        public CampProfile ()
+        {
+            this.CreateMap<Camp, CampModel>().
+            ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName));
+        }
+    }
+}
